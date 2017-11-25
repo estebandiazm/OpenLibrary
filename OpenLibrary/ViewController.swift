c21e4ba21e966f9d058d676e0de8fbfca4c07495
Juan Esteban Diaz Montejo <juanesteban@MacBook-Pro-de-Juan.local>
2017-11-24 22:03:07 -0500

    Add project

--- OpenLibrary/ViewController.swift
+++ OpenLibrary/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate  {
 
+    
+    @IBOutlet weak var ISBNField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var cleanButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func callService(isbnBook:NSString) -> NSString{
+        let paht = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+(isbnBook as String)
+        let url = NSURL(string:paht)
+        let datos:NSData? = NSData(contentsOf: url! as URL)
+        let texto = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+        return texto!
+    }
+    
+    @IBAction func searchBook(_ sender: Any) {
+        let result = callService(isbnBook: ISBNField.text! as NSString)
+        textView.text = result as String!
+    }
+    
+    @IBAction func cleanISBN(_ sender: Any) {
+        ISBNField.text = ""
+    }
+    
+    @IBAction func searchKeyboard(_ sender: Any) {
+        let result = callService(isbnBook: ISBNField.text! as NSString)
+        textView.text = result as String!
+    }
+    
+    
 }
 
